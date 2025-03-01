6f09290fcecec7d56c54cd9ee403d0e11f68afbc
Levi Zim <rsworktech@outlook.com>
2025-03-01 19:49:57 +0800

    basic skeleton for frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,60 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './graph.css'
+import cytoscape from 'cytoscape';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//     <div id="topbar"></div>
+//     <div id="graph"></div>
+//     <div id="sidebar"></div>
+// `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const graph = cytoscape({
+  container: document.getElementById('graph'),
+  elements: [ // list of graph elements to start with
+    { // node a
+      data: { id: 'tokio-1.0.0', package: "tokio", version: "1.0.0" },
+    },
+    { // node b
+      data: { id: 'rand-0.9.0', package: "rand", version: "0.9.0" }
+    },
+    { // node a
+      data: { id: 'tokio-1.1.0', package: "tokio", version: "1.0.0" },
+    },
+    { // node b
+      data: { id: 'rand-1.0.0', package: "rand", version: "0.9.0" }
+    },
+    { // edge ab
+      data: { id: 'tokio-1.0.0 depends on rand-0.9.0', source: 'tokio-1.0.0', target: 'rand-0.9.0' }
+    },
+    { // edge ab
+      data: { id: 'tokio-1.1.0 depends on rand-1.0.0', source: 'tokio-1.1.0', target: 'rand-1.0.0' }
+    }
+  ],
+  style: [ // the stylesheet for the graph
+    {
+      selector: 'node',
+      style: {
+        'background-color': '#666',
+        'label': 'data(id)'
+      }
+    },
+  ]
+});
+
+graph.on('select', 'node', (event) => {
+  const data = event.target.data();
+  document.getElementById("sidebar")!.innerHTML = `
+    <p>name: ${data.package}</p>
+    <p>version: ${data.version}</p>
+  `
+});
+
+document.getElementById("reset")!.addEventListener("click", () => {
+  graph.reset()
+})
+
+document.getElementById("layout")!.addEventListener("click", () => {
+  const layout = graph.layout({ name: "random" });
+  layout.run()
+})
+
